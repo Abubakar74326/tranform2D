90d18cac4d3d63b7355a3ddaffb4445f3b8767c1
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-28 06:23:45 -0400

    shearing x,y in one function

--- functionalities.py
+++ functionalities.py
@@ -1,31 +1,31 @@
 import math
 
 import matplotlib.pyplot as plt
-from pylab import imread,imshow,show
+from pylab import imread, imshow, show
 
 
 class Transformation:
 
-    def ploting(self,x,y,xnew,ynew, title_):
+    def ploting(self, x, y, xnew, ynew, title_):
 
         plt.grid()
         plt.title(title_)
         # st=input("enter the tittle")
         plt.xlabel("X-Axis")
         plt.ylabel("Y-Axis")
+        plt.xlim(0,10)
+        plt.ylim(0,10)
         xpoints = [x, xnew]
         ypoints = [y, ynew]
 
-        plt.plot(xpoints, ypoints, "o")
-        plt.savefig('plot.png')
+        plt.plot(x, y, "o")
+        plt.plot(xpoints, ypoints, "*")
+        # plt.savefig('plot.png')
         plt.show()
 
         # image = plt.imread('/home/revolution/pictures.png')
         # plt.imshow(image)
         # plt.show()
-
-
-
 
     def rotation(self, x, y, angel, graph=True):
         """
@@ -39,12 +39,11 @@ class Transformation:
         ynew = ((y * (math.cos(angel))) + (x * (math.sin(angel))))
 
         if graph == True:
-            self.ploting(x, y, xnew, ynew, 'rot')
+            self.ploting(x, y, xnew, ynew,'rotation')
 
         return xnew, ynew
 
-
-    def translation(self,x,y,Tx,Ty,graph=True):
+    def translation(self, x, y, Tx, Ty, graph=True):
         """
         summary line:
         In 2D translation object moves from one position to another in a two dimensional plane.
@@ -60,16 +59,16 @@ class Transformation:
         ynew = y + Ty
 
         if graph == True:
-            self.ploting(x, y, xnew, ynew, 'translation')
+            self.ploting(x, y, xnew, ynew, 'translation', )
+        else:
+            return xnew, ynew
 
-        return xnew, ynew
-
-
-    def scaling(self, x,y,Sx, Sy):
+    def scaling(self, x, y, Sx, Sy, graph=True):
         """
         summary line:
         In scaling we modifying or altering the size of objects.
         parameter:
+        :param graph:
         :param x:initial x coordinates of the object
         :param y:initial y coordinates of the object
         :param Sx:Scaling factor for X-axis
@@ -79,21 +78,12 @@ class Transformation:
         """
         xnew = x * Sx
         ynew = y * Sy
+        if graph==True:
+            self.ploting(x,y,Sx,Sy,'Scaling')
         return xnew, ynew
 
 
-    def sheringX(self,x,y,Shx):
-        """
-
-        :param x: initial x coordinates of the object
-        :param y: initial x coordinates of the object
-        :param Shx:Shearing parameter towards X direction
-        :return:return New coordinates of the object after shearing
-        """
-        return x + (Shx * y)
-
-
-    def sheringY(self,y,x,Shy):
+    def shering(self, x, y, Shx, Shy, graph=True):
         """
 
         :param y:  initial y coordinates of the object
@@ -101,17 +91,13 @@ class Transformation:
         :param Shy: Shearing parameter towards Y direction
         :return: return New coordinates of the object after shearing
         """
-        return y + (Shy * x)
-
-
+        if graph==True:
+            self.ploting(x,y,Shx,Shy, 'Shearing')
+        return y + (Shy * x),x + (Shx * y)
 
 
 if __name__ == "__main__":
     t = Transformation()
-    xnew,ynew = t.rotation(1,2,-45, graph=True)
-    print(xnew,ynew)
+    xnew, ynew = t.rotation(1, 2, -45, graph=True)
+    print(xnew, ynew)
     # Transformation.ploting(1, 2, xnew, ynew )
-
-
-
-
