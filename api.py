8dac9e4054450402817708a5284728b59a5a3dce
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-28 01:16:16 -0400

    object of class from outside the functions and new docstring

--- api.py
+++ api.py
@@ -1,30 +1,37 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
+import transformations
+import visualization
 from code import Transformation
 
 app = Flask(__name__)
+#OBJECT of Class
+transformer = Transformation()
 
-#post method for rotate
+# post method for rotate
 @app.route("/rotate/", methods=['POST'])
 def rotate():
     """
-
+    This function take x,y,angle value as an input and operation implemented by rotation function calling of class
+    Transformation and return the new x,y coordinates.
     :return: it returns the new coordinates after rotational transformation in post method
     """
     x = float(request.form['x'])
     y = float(request.form['y'])
     angle = float(request.form['angle'])
 
-    transformer = Transformation()
+
 
     x, y = transformer.rotation(x, y, angle)
     return {'x': x, 'y': y}
 
-#Post method for translational
+
+# Post method for translational
 @app.route("/translation/", methods=['POST'])
 def translation():
     """
-
+    This function take initial  x,y coordinates an as an input and operation implemented along_with add new coordinates value of Tx,Ty by translation function calling of class
+    Transformation and return the new x,y coordinates.
     :return: it return new coordinates of object after translation
     """
     x = float(request.form['x'])
@@ -32,43 +39,58 @@ def translation():
     Tx = float(request.form['Tx'])
     Ty = float(request.form['Ty'])
 
-    transformer = Transformation()
+
     x, y = transformer.translation(x, y, Tx, Ty)
     return {'x': x, 'y': y}
 
-#Post method for scaling
+
+# Post method for scaling
 @app.route("/scaling/", methods=['POST'])
 def scaling():
     """
-
+    This function take initial  x,y coordinates an as an input and operation implemented by multiplying new coordinates value of Sx,Sy by scaling  function calling from class
+    Transformation and return the new x,y coordinates.
     :return: it return new coordinates of object after scaling
     """
     x = float(request.form['x'])
     y = float(request.form['y'])
     Sx = float(request.form['Sx'])
     Sy = float(request.form['Sy'])
-    transformer = Transformation()
+
     x, y = transformer.scaling(x, y, Sx, Sy)
     return {'x': x, 'y': y}
 
-#Post method for shering
+
+# Post method for shering
 @app.route("/shearing/", methods=['POST'])
 def shering():
     """
-
+     This function take initial  x,y coordinates an as an input and operation implemented by shearing  function calling from class
+    Transformation and return the new x,y coordinates.
     :return: it return new coordinates of object after shering transformation
     """
     x = float(request.form['x'])
     y = float(request.form['y'])
     Shx = float(request.form['Sx'])
     Shy = float(request.form['Sy'])
-    transformer = Transformation()
+
     x = transformer.sheringX(x, y, Shx)
-    """functional call for x coordinate"""
     y = transformer.sheringY(y, x, Shy)
-    """functional call for y coordinate"""
 
     return {'x': x, 'y': y}
+
+#ploting function
+@app.route('/plot')
+def plot():
+    """
+     this is self practice to take the translation graph as an output from ploting function of visualization file by api
+    :return:
+    """
+    x = 3
+    y = 3
+    xnew, ynew = transformations.translation(x, y, 2, 4)
+    plot, url = visualization.ploting(x, y, xnew, ynew)
+    return render_template(plot, plot_url=url)
 
 
 if __name__ == "__main__":
