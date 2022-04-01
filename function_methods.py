ace462db8f72170b6fb91fb6f4df81d7f5996c0c
muhammad abubakar <mabubakarbssg@gmail.com>
2022-04-01 07:08:36 -0400

    fun for rect

--- function_methods.py
+++ function_methods.py
@@ -1,14 +1,20 @@
 import matplotlib
+import matplotlib.backend_tools
 import matplotlib.pyplot as plt
 
 def rec(le,wi):
     fig = plt.figure()
     ax = fig.add_subplot()
-    pl
     rect = matplotlib.patches.Rectangle((1, 1), le, wi,color='green')
     ax.add_patch(rect)
+    plt.savefig('plot.png')
     plt.xlim([0, 10])
     plt.ylim([0, 10])
-
     plt.show()
+
+def tri():
+
+
+
+
 rec(3,4)
