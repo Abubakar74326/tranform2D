5986e2ad137c59b490d608267bbe188ce7335ad5
muhammad abubakar <mabubakarbssg@gmail.com>
2022-04-03 11:43:28 -0400

    functions of rect,tri,circle

--- function_methods.py
+++ function_methods.py
@@ -1,8 +1,8 @@
 import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
-#from matplotlib.patches import  Rectangle
-#import matplotlib.backend_tools
+from matplotlib.patches import Circle
+from matplotlib.patches import Rectangle
 
 
 
@@ -28,5 +28,16 @@ def tri():
     plt.ylim([0, 10])
     plt.show()
 
-#tri()
-rec(2,3)
+def cir(radius):
+    fig =plt.figure()
+    ax= fig.add_subplot()
+    circle = Circle((0,0),radius, color = "green")
+    ax.add_patch(circle)
+    plt.xlim([-10,10])
+    plt.ylim([-10,10])
+    plt.show()
+
+
+#cir(5)
+tri()
+#rec(2,3)
