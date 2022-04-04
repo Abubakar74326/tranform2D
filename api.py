a2a58b08474bc118b17d835058565dc19cee41b8
muhammad abubakar <mabubakarbssg@gmail.com>
2022-04-03 23:14:44 -0400

    api with docstring

--- api.py
+++ api.py
@@ -6,24 +6,43 @@ app = Flask(__name__)
 #post method for rect
 @app.route("/rectangle")
 def rectangle():
+    """
+    call the rectangle function from function_method file
+    take length and width from user
+    :return: plot the  image graph save in current directory
+    """
     le = float(request.form['le'])
     wi = float(request.form['wi'])
     function_methods.rec(le,wi)
     return send_from_directory('.','plot.png')
 
-@app.route("/tri")
+@app.route("/triangle")
 def triangle():
+    """
+    call the triangle function from function_method file
+    :return: plot the  image graph save in current directory
+    """
     function_methods.tri()
     return send_from_directory('.', 'plot.png')
 
 @app.route("/circle")
 def circle():
+    """
+    call the circle function from function_method file
+    take radius from user
+    :return: plot the  image graph save in current directory
+    """
     radius = float(request.form['radius'])
     function_methods.cir(radius)
     return send_from_directory('.', 'plot.png')
 
 @app.route("/square")
 def square():
+    """
+    call the square function from function_method file
+    take same length and width from user
+    :return: plot the  image graph save in current directory
+    """
     le = float(request.form['le'])
     wi = float(request.form['wi'])
     function_methods.square(le,wi)
